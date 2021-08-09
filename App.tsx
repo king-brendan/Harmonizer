9cf000beb23a82050649724103434113fc335a9f
Brendan King <brendanking@Brendans-MacBook-Pro.local>
2021-08-09 11:07:15 -0400

    cleaned up code

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 import { Audio } from "expo-av";
 
 const App = () => {
   const [sound, setSound] = React.useState<Audio.Sound | undefined>();
   const [backingTrack, setBacking] = React.useState<Audio.Sound | undefined>();
   const [backingPlaying, setBackingPlaying] = React.useState(false);
-  const [currentChord, changeCurrentChord] = React.useState("none");
 
   async function playSound(file: string) {
     if (file === "none") {
@@ -50,17 +47,7 @@ const App = () => {
     ]);
   }
 
-  /*
-  React.useEffect(() => {
-    return sound
-      ? () => {
-          console.log("Unloading Sound");
-          sound.unloadAsync();
-        }
-      : undefined;
-  }, [sound]);
-  */
-
+  // Build chords here
   const cMajor = () => playSounds("C.mp3", "E.mp3", "G.mp3", "highC.mp3");
   const dMinor = () => playSounds("D.mp3", "F.mp3", "A.mp3", "highC.mp3");
   const eMinor = () => playSounds("E.mp3", "G.mp3", "B.mp3", "highE.mp3");
@@ -71,10 +58,27 @@ const App = () => {
   const aMinor = () =>
     playSounds("A.mp3", "highC.mp3", "highE.mp3", "highA.mp3");
 
+  const chords = [
+    cMajor,
+    dMinor,
+    eMinor,
+    fMajorInv,
+    fMajor,
+    fMajorSeven,
+    gMajor,
+    aMinor,
+  ];
+
+  const randomChord = () => {
+    chords[Math.floor(Math.random() * chords.length)]();
+  };
+
   return (
     <View style={styles.container}>
+      <h1>Harmonizer</h1>
       <View style={styles.drumsBox}>
         <Button
+          color="#7F00FF"
           onPress={() => {
             setBackingPlaying(true);
             playBacking("drums.mp3");
@@ -82,6 +86,7 @@ const App = () => {
           title="Play Drums"
         />
         <Button
+          color="#7F00FF"
           onPress={async () => {
             setBackingPlaying(false);
             await backingTrack?.stopAsync();
@@ -90,7 +95,7 @@ const App = () => {
         />
       </View>
       <View style={styles.buttonsBox}>
-        <Button onPress={cMajor} title="Home" />
+        <Button color="#07c700" onPress={cMajor} title="Home" />
         <Button onPress={dMinor} title="Wistful" />
         <Button onPress={eMinor} title="Heartache" />
         <Button onPress={fMajorInv} title="Next Door" />
@@ -99,92 +104,30 @@ const App = () => {
         <Button onPress={gMajor} title="Jubilant" />
         <Button onPress={aMinor} title="In Despair" />
       </View>
+      <View style={styles.otherButtonsBox}>
+        <Button color="#f44336" onPress={randomChord} title="Suprise Me" />
+      </View>
     </View>
   );
-
-  /*
-  return (
-    <View style={styles.container}>
-      <Button
-        onPress={() => {
-          setBackingPlaying(true);
-          playBacking("drums.mp3");
-        }}
-        title="Play Drums"
-      />
-      <Button
-        onPress={async () => {
-          setBackingPlaying(false);
-          await backingTrack?.stopAsync();
-        }}
-        title="Stop Drums"
-      />
-      <Button onPress={cMajor} title="C major" />
-      <Button
-        onPress={() => playSounds("D.mp3", "F.mp3", "A.mp3", "highC.mp3")}
-        title="D minor 7"
-      />
-      <Button
-        onPress={() => playSounds("D.mp3", "F.mp3", "A.mp3", "highD.mp3")}
-        title="D minor"
-      />
-      <Button
-        onPress={() => playSounds("D.mp3", "F.mp3", "G#.mp3", "highC.mp3")}
-        title="D minor 7 flat 5"
-      />
-      <Button
-        onPress={() => playSounds("E.mp3", "G.mp3", "B.mp3", "highE.mp3")}
-        title="E minor"
-      />
-      <Button
-        onPress={() => playSounds("E.mp3", "G.mp3", "B.mp3", "highD.mp3")}
-        title="E minor 7"
-      />
-      <Button
-        onPress={() => playSounds("E.mp3", "G#.mp3", "B.mp3", "D.mp3")}
-        title="E 7"
-      />
-      <Button
-        onPress={() => playSounds("C.mp3", "F.mp3", "A.mp3", "highC.mp3")}
-        title="F major 3rd inversion"
-      />
-      <Button
-        onPress={() => playSounds("F.mp3", "A.mp3", "C.mp3", "highF.mp3")}
-        title="F major"
-      />
-      <Button
-        onPress={() => playSounds("F.mp3", "A.mp3", "C.mp3", "highE.mp3")}
-        title="F major 7"
-      />
-      <Button
-        onPress={() => playSounds("G.mp3", "B.mp3", "highD.mp3", "highG.mp3")}
-        title="G major"
-      />
-      <Button
-        onPress={() => playSounds("D.mp3", "F.mp3", "G.mp3", "B.mp3")}
-        title="G 7"
-      />
-      <Button
-        onPress={() => playSounds("E.mp3", "A.mp3", "C.mp3", "highC")}
-        title="A minor"
-      />
-    </View>
-  );
-  */
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ffe8f7",
     alignItems: "center",
     justifyContent: "center",
   },
   drumsBox: {
     flex: 1,
+    justifyContent: "space-evenly",
   },
   buttonsBox: {
     flex: 2,
+    justifyContent: "space-evenly",
+  },
+  otherButtonsBox: {
+    flex: 1,
   },
 });
 
